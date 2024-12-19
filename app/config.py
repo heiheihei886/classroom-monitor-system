825cbf6cb705922d4d3d3d698e2681b4dc69ebf7
heiheihei886 <thrf3f@outlook.com>
2024-12-19 14:58:11 +0800

    fix token message and login

--- app/config.py
+++ app/config.py
@@ -1,3 +1,5 @@
+import os
+
 DIALECT = 'mysql'
 DRIVER = 'pymysql'
 USERNAME = "root"
@@ -8,3 +10,5 @@ DATABASE = 'classroom_student_monitoring_system'
 SQLALCHEMY_DATABASE_URI = '{}+{}://{}:{}@{}:{}/{}?charset=utf8'.format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT,
                                                                        DATABASE)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SECRET_KEY = os.environ.get('SECRET_KEY') or '1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p'
--- app/run.py
+++ app/run.py
@@ -1,7 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from exts import db
 import config
+import jwt
+import datetime
 
 app = Flask(__name__)
 CORS(app, origins="http://localhost:9527")
@@ -30,19 +32,76 @@ def create_blueprint():
     app.register_blueprint(student, url_prefix='/student')
 
 
-@app.route('/login', methods=['GET'])
+# @app.route('/login', methods=['GET'])
+# def login():
+#     username = request.args.get("username")  # 使用 args 获取查询参数
+#     password = request.args.get("password")
+#     if not username:
+#         return "Please enter username"
+#     if not password:
+#         return "Please enter password"
+#     user = User.query.filter_by(username=username).first()
+#     if not user or user.password != password:  # 假设未使用加密存储
+#         return "Username or password incorrect"
+#     return "sss"
+
+@app.route('/login', methods=['POST'])
 def login():
-    username = request.form.get("username")
-    password = request.form.get("password")
+    data = request.get_json()
+    print('获取的数据')
+    print(data)# 获取 JSON 数据
+    username = data.get("username")
+    password = data.get("password")
+    print(username, password)
     if not username:
-        return "Please enter username"
+        return jsonify({"error": "Please enter username"}), 400
     if not password:
-        return "Please enter password"
+        return jsonify({"error": "Please enter password"}), 400
     user = User.query.filter_by(username=username).first()
-    print(user)
-    if not user:
-        return "Username or password incorrect"
-    return "sss"
+    if not user or user.password != password:  # 假设未使用加密存储
+        return jsonify({"error": "Username or password incorrect"}), 401
+
+    # 生成 token
+    payload = {
+        'user_id': user.id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1)  # token 过期时间为1天
+    }
+    token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+    print('成功生成token' + token)
+
+    return jsonify({"code": 200, "message": "Login successful", "token": token}), 200
+
+
+@app.route('/get_user_info', methods=['GET'])
+def get_user_info():
+    # token = request.headers.get('Authorization')
+    token = request.args.get('token')
+    if not token:
+        return jsonify({"error": "Token is missing"}), 401
+
+    try:
+        payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        user_id = payload['user_id']
+        print(f'请求的的id{user_id}')
+        user = User.query.get(user_id)
+        if not user:
+            return jsonify({"error": "User not found"}), 404
+
+        # 返回用户信息
+        user_info = {
+            'code': 200,  # 添加 code 字段
+            'data': {
+                'id': user.id,
+                'username': user.username,
+                'roles': 'admin'
+            }
+        }
+        return jsonify(user_info), 200
+
+    except jwt.ExpiredSignatureError:
+        return jsonify({"error": "Token has expired"}), 401
+    except jwt.InvalidTokenError:
+        return jsonify({"error": "Invalid token"}), 401
 
 
 if __name__ == '__main__':
