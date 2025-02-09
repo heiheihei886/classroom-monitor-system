94d9440f14bb7b6ea5f72501f2b33c5bab1915d9
LMY928 <142732583+LMY928@users.noreply.github.com>
2025-02-09 23:32:30 +0800

    Update BoxCard.vue

--- src/views/dashboard/editor/components/BoxCard.vue
+++ src/views/dashboard/editor/components/BoxCard.vue
@@ -5,7 +5,7 @@
     </div>
     <div style="position:relative;">
       <pan-thumb :image="avatar" class="panThumb" />
-      <mallki class-name="mallki-text" text="Attendence Tracker" />
+      <mallki class-name="mallki-text" text="PROOF OF CICD" />
       <!-- <div class="progress-item">
         <span>SWE 5001</span>
         <el-progress :percentage="12" />
